6581bad9b1315e4d3c9dc2a8549a928a4eacb22f
Kshitij Koyande <koyande72@gmail.com>
2025-04-01 13:24:20 +0000

    Revert "display: Add custom screen resolution setting"
    This reverts commit 8080496b0ce3d1fd22ee5db324595fe8ba52d553.

--- src/com/android/settings/display/ScreenResolutionController.java
+++ src/com/android/settings/display/ScreenResolutionController.java
@@ -88,9 +88,7 @@ public class ScreenResolutionController extends BasePreferenceController {
 
     @Override
     public int getAvailabilityStatus() {
-        return !(mContext.getResources().getBoolean(
-                R.bool.config_show_custom_screen_resolution_switch)) &&
-                (checkSupportedResolutions()) ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+        return (checkSupportedResolutions()) ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 
     @Override
